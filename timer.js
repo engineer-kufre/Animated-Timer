bf73daca9d81f545d1d3fb0a42740d79fb3535bf
engineer-kufre <engineer_kufre@yahoo.com>
2020-02-04 14:02:14 +0100

    updated js

--- timer.js
+++ timer.js
@@ -1,4 +1,5 @@
-const timer_span = document.getElementById('base-timer-label')
+const timer_span = document.getElementById('base-timer-label');
+const progress_path = document.getElementById('base-timer-path-remaining');
 
 function formatTimeLeft() {
     if (timeLeft >= 0) {
@@ -17,13 +18,45 @@ let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 formatTimeLeft();
 
-let timeInterval = null;
+let timerInterval = null;
 
 function startTimer() {
-    timeInterval = setInterval(() => {
+    timerInterval = setInterval(() => {
         timePassed++;
         timeLeft = TIME_LIMIT - timePassed;
         timer_span.innerHTML = formatTimeLeft(timeLeft);
+
+        setCircleDasharray();
     }, 1000);
 }
 startTimer();
+
+function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+const FULL_DASH_ARRAY = 283;
+
+function setCircleDasharray() {
+    const circleDasharray = `${(
+        calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+    document
+    .getElementById('base-timer-path-remaining')
+    .setAttribute('stroke-dasharray', circleDasharray);
+}
+
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+function setRemainingPathColor(timeLeft) {
+    if (timeLeft <= ALERT_THRESHOLD) {
+        progress_path.classList.remove('warning');
+        progress_path.classList.add('alert');
+    } else if (timeLeft <= WARNING_THRESHOLD) {
+        progress_path.classList.remove('info');
+        progress_path.classList.add('warning');
+    }
+}
+setRemainingPathColor();
