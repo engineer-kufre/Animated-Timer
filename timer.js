ad1b842d69a86ea25959cdd3ba8b83a7c39b53f3
engineer-kufre <engineer_kufre@yahoo.com>
2020-02-04 12:54:25 +0100

    updated js

--- timer.js
+++ timer.js
@@ -1,15 +1,29 @@
 const timer_span = document.getElementById('base-timer-label')
 
 function formatTimeLeft() {
-    const minutes = Math.floor(timeLeft / 60);
-    let seconds = timeLeft % 60;
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
-    }
-    timer_span.innerHTML = `${minutes}:${seconds}`;
+    if (timeLeft >= 0) {
+       const minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        }
+        return `${minutes}:${seconds}`; 
+    } else return `0:00`;
+    
 }
 
-const TIME_LIMIT = 20;
+const TIME_LIMIT = 21;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 formatTimeLeft();
+
+let timeInterval = null;
+
+function startTimer() {
+    timeInterval = setInterval(() => {
+        timePassed++;
+        timeLeft = TIME_LIMIT - timePassed;
+        timer_span.innerHTML = formatTimeLeft(timeLeft);
+    }, 1000);
+}
+startTimer();
