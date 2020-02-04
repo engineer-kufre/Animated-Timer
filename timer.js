38558899eddcf5f317ad5bc0d9ae272bd6090d53
engineer-kufre <engineer_kufre@yahoo.com>
2020-02-04 17:32:46 +0100

    edited js

--- timer.js
+++ timer.js
@@ -1,6 +1,8 @@
-const timer_span = document.getElementById('base-timer-label');
-const progress_path = document.getElementById('base-timer-path-remaining');
+//cashing the DOM
+const timer_span = document.getElementById('base-timer-label'); //for countdown
+const progress_path = document.getElementById('base-timer-path-remaining'); //for path color animation
 
+//calculate time left in minutes and seconds
 function formatTimeLeft() {
     if (timeLeft >= 0) {
        const minutes = Math.floor(timeLeft / 60);
@@ -13,31 +15,34 @@ function formatTimeLeft() {
     
 }
 
-const TIME_LIMIT = 21;
+const TIME_LIMIT = 62; //set timer duration
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 formatTimeLeft();
 
 let timerInterval = null;
 
+//timer count interval, display count, display color and dasharray. All resolved every 1 second
 function startTimer() {
     timerInterval = setInterval(() => {
         timePassed++;
         timeLeft = TIME_LIMIT - timePassed;
         timer_span.innerHTML = formatTimeLeft(timeLeft);
-
+        setRemainingPathColor();
         setCircleDasharray();
     }, 1000);
 }
-startTimer();
+startTimer(); //ignition
 
+//calculate fraction of time left
 function calculateTimeFraction() {
     const rawTimeFraction = timeLeft / TIME_LIMIT;
     return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 
-const FULL_DASH_ARRAY = 283;
+const FULL_DASH_ARRAY = 283; //full dash array covers 360deg on the timer face
 
+//calc the amount of circle dash array relative to fraction of time left
 function setCircleDasharray() {
     const circleDasharray = `${(
         calculateTimeFraction() * FULL_DASH_ARRAY
@@ -47,10 +52,12 @@ function setCircleDasharray() {
     .setAttribute('stroke-dasharray', circleDasharray);
 }
 
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
+//set warning and alert thresholds as fractions of the initially set timer duration. Determines point of color change
+const WARNING_THRESHOLD = TIME_LIMIT / 2;
+const ALERT_THRESHOLD = TIME_LIMIT / 4;
 
-function setRemainingPathColor(timeLeft) {
+//add and remove classes to change path color
+function setRemainingPathColor() {
     if (timeLeft <= ALERT_THRESHOLD) {
         progress_path.classList.remove('warning');
         progress_path.classList.add('alert');
@@ -59,4 +66,3 @@ function setRemainingPathColor(timeLeft) {
         progress_path.classList.add('warning');
     }
 }
-setRemainingPathColor();
